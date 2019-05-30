076020bd11cb5fd0e0622ead8e0294a8ebf0bef0
Marc Campbell <marc.e.campbell@gmail.com>
2019-05-30 04:22:11 +0000

    Don't run as root

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+CURRENT_UID=${uid:-9999}
+
+echo "Current UID : $CURRENT_UID"
+useradd --shell /bin/bash -u $CURRENT_UID -o -c "" -m docker
+export HOME=/home/docker
+
+# Execute process
+exec /usr/local/bin/gosu docker /schemahero "$@"
