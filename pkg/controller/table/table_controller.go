fbdd5c9faff36071392493722c2bfbc134bbdf7b
Marc Campbell <marc.e.campbell@gmail.com>
2019-06-03 21:30:00 +0000

    Prevent apply when already exists

--- pkg/controller/table/table_controller.go
+++ pkg/controller/table/table_controller.go
@@ -261,22 +261,29 @@ func (r *ReconcileTable) ensureTableConfigMap(database *databasesv1alpha1.Databa
 	tableData := make(map[string]string)
 	tableData["table.yaml"] = string(b)
 
-	configMap := corev1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      table.Name,
-			Namespace: database.Namespace,
-		},
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "v1",
-			Kind:       "ConfigMap",
-		},
-		Data: tableData,
-	}
-	if err := controllerutil.SetControllerReference(table, &configMap, r.scheme); err != nil {
-		return err
-	}
-	err = r.Create(context.TODO(), &configMap)
-	if err != nil {
+	existingConfigMap := corev1.ConfigMap{}
+	if err := r.Get(context.TODO(), types.NamespacedName{Name: table.Name, Namespace: database.Namespace}, &existingConfigMap); err != nil {
+		if kuberneteserrors.IsNotFound(err) {
+			configMap := corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      table.Name,
+					Namespace: database.Namespace,
+				},
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "ConfigMap",
+				},
+				Data: tableData,
+			}
+			if err := controllerutil.SetControllerReference(table, &configMap, r.scheme); err != nil {
+				return err
+			}
+			err = r.Create(context.TODO(), &configMap)
+			if err != nil {
+				return err
+			}
+		}
+
 		return err
 	}
 
@@ -320,61 +327,70 @@ func (r *ReconcileTable) ensureTablePod(database *databasesv1alpha1.Database, ta
 	labels := make(map[string]string)
 	labels["schemahero-role"] = "table"
 
-	pod := corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      fmt.Sprintf("%s-apply", table.Name),
-			Namespace: database.Namespace,
-			Labels:    labels,
-		},
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "v1",
-			Kind:       "Pod",
-		},
-		Spec: corev1.PodSpec{
-			NodeSelector:       nodeSelector,
-			ServiceAccountName: database.Name,
-			RestartPolicy:      corev1.RestartPolicyOnFailure,
-			Containers: []corev1.Container{
-				{
-					Image:           imageName,
-					ImagePullPolicy: corev1.PullAlways,
-					Name:            table.Name,
-					Args: []string{
-						"apply",
-						"--driver",
-						driver,
-						"--uri",
-						connectionURI,
-						"--spec-file",
-						"/specs/table.yaml",
-					},
-					VolumeMounts: []corev1.VolumeMount{
+	existingPod := corev1.Pod{}
+	if err := r.Get(context.TODO(), types.NamespacedName{Name: fmt.Sprintf("%s-apply", table.Name), Namespace: database.Namespace}, &existingPod); err != nil {
+		if kuberneteserrors.IsNotFound(err) {
+			pod := corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      fmt.Sprintf("%s-apply", table.Name),
+					Namespace: database.Namespace,
+					Labels:    labels,
+				},
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "v1",
+					Kind:       "Pod",
+				},
+				Spec: corev1.PodSpec{
+					NodeSelector:       nodeSelector,
+					ServiceAccountName: database.Name,
+					RestartPolicy:      corev1.RestartPolicyOnFailure,
+					Containers: []corev1.Container{
 						{
-							Name:      "specs",
-							MountPath: "/specs",
+							Image:           imageName,
+							ImagePullPolicy: corev1.PullAlways,
+							Name:            table.Name,
+							Args: []string{
+								"apply",
+								"--driver",
+								driver,
+								"--uri",
+								connectionURI,
+								"--spec-file",
+								"/specs/table.yaml",
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "specs",
+									MountPath: "/specs",
+								},
+							},
 						},
 					},
-				},
-			},
-			Volumes: []corev1.Volume{
-				{
-					Name: "specs",
-					VolumeSource: corev1.VolumeSource{
-						ConfigMap: &corev1.ConfigMapVolumeSource{
-							LocalObjectReference: corev1.LocalObjectReference{
-								Name: table.Name,
+					Volumes: []corev1.Volume{
+						{
+							Name: "specs",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: table.Name,
+									},
+								},
 							},
 						},
 					},
 				},
-			},
-		},
-	}
-	if err := controllerutil.SetControllerReference(table, &pod, r.scheme); err != nil {
-		return err
-	}
-	err := r.Create(context.TODO(), &pod)
-	if err != nil {
+			}
+			if err := controllerutil.SetControllerReference(table, &pod, r.scheme); err != nil {
+				return err
+			}
+			err := r.Create(context.TODO(), &pod)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
 		return err
 	}
 
