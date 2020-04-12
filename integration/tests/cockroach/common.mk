898b888a470c199cb6c8818151111f9212bea10e
Marc Campbell <marc.e.campbell@gmail.com>
2020-04-11 21:41:24 -0700

    Update

--- integration/tests/cockroach/common.mk
+++ integration/tests/cockroach/common.mk
@@ -8,6 +8,7 @@ URI := postgres://schemahero@127.0.0.1:26257/schemahero?sslmode=disable
 run:
 	# Fixtures
 	docker pull cockroachdb/cockroach:v19.2.5
+  dcoker tag cockroachdb/cockroach:v19.2.5 $(DATABASE_IMAGE_NAME)
 	@-docker rm -f $(DATABASE_CONTAINER_NAME) > /dev/null 2>&1 ||:
 	docker run -p 26257:26257 --rm -d \
 		--name $(DATABASE_CONTAINER_NAME) \
