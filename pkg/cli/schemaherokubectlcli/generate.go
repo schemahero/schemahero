1970e47a0fca485147a53b080e607e8aced3a6f4
Marc Campbell <marc.e.campbell@gmail.com>
2020-06-11 16:24:20 -0700

    Client side generate

--- pkg/cli/schemaherokubectlcli/generate.go
+++ pkg/cli/schemaherokubectlcli/generate.go
@@ -1,17 +1,13 @@
 package schemaherokubectlcli
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"os"
 
-	databasesclientv1alpha4 "github.com/schemahero/schemahero/pkg/client/schemaheroclientset/typed/databases/v1alpha4"
 	"github.com/schemahero/schemahero/pkg/generate"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 func GenerateCmd() *cobra.Command {
@@ -26,52 +22,22 @@ func GenerateCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			v := viper.GetViper()
 
-			cfg, err := config.GetConfig()
-			if err != nil {
-				return err
+			if v.GetString("database") != "" {
+				// use a deployed database object
+				// and create a pod in the cluster to access it
+
+				// TODO
+				return errors.New("not implemented")
 			}
-
-			databasesClient, err := databasesclientv1alpha4.NewForConfig(cfg)
-			if err != nil {
-				return err
-			}
-
-			namespace := v.GetString("namespace")
-			if namespace == "" {
-				namespace = corev1.NamespaceDefault
-			}
-
-			ctx := context.Background()
-			database, err := databasesClient.Databases(namespace).Get(ctx, v.GetString("database"), metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			driver := ""
-			connectionURI := ""
-
-			if database.Spec.Connection.Postgres != nil {
-				driver = "postgres"
-				connectionURI = database.Spec.Connection.Postgres.URI.Value
-			} else if database.Spec.Connection.Mysql != nil {
-				driver = "mysql"
-				connectionURI = database.Spec.Connection.Mysql.URI.Value
-			} else if database.Spec.Connection.CockroachDB != nil {
-				driver = "cockroachdb"
-				connectionURI = database.Spec.Connection.CockroachDB.URI.Value
-			}
-
-			// TODO support Vault
-			// TODO support non URI connection params
 
 			g := generate.Generator{
-				Driver:    driver,
-				URI:       connectionURI,
-				DBName:    database.Name,
+				Driver:    v.GetString("driver"),
+				URI:       v.GetString("uri"),
+				DBName:    v.GetString("dbname"),
 				OutputDir: v.GetString("output-dir"),
 			}
-
 			return g.RunSync()
+
 		},
 	}
 
@@ -81,7 +47,12 @@ func GenerateCmd() *cobra.Command {
 		cwd = "."
 	}
 
-	cmd.Flags().StringP("database", "d", "", "database name to generate table yaml for")
+	cmd.Flags().String("database", "", "database name to generate table yaml for")
+
+	cmd.Flags().String("uri", "", "connection string uri")
+	cmd.Flags().String("driver", "", "name of the database driver to run")
+	cmd.Flags().String("dbname", "", "schemahero database name to write in the yaml")
+
 	cmd.Flags().String("output-dir", cwd, "directory to write schema files to")
 
 	return cmd
