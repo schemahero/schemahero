147d69382e0e1ef72b06ee2125f4d25d1230e099
Marc Campbell <marc.e.campbell@gmail.com>
2020-10-07 05:57:20 -0700

    Support all K8s auth providers
    Signed-off-by: Marc Campbell <marc.e.campbell@gmail.com>

--- cmd/kubectl-schemahero/main.go
+++ cmd/kubectl-schemahero/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/schemahero/schemahero/pkg/cli/schemaherokubectlcli"
+import (
+	"github.com/schemahero/schemahero/pkg/cli/schemaherokubectlcli"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+)
 
 func main() {
 	schemaherokubectlcli.InitAndExecute()
