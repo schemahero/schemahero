2a136610c73b1a816c3d7aa4b2da7ae09a5797f2
Matthew L Daniel <md@stoi.cc>
2021-03-16 21:51:32 -0700

    Do not use Sprintf for passwords into URLs
    Signed-off-by: Matthew L Daniel <md@stoi.cc>

--- integration/tests/postgres/common.mk
+++ integration/tests/postgres/common.mk
@@ -2,7 +2,8 @@ SHELL := /bin/bash
 DATABASE_IMAGE_NAME := schemahero/database
 DATABASE_CONTAINER_NAME := schemahero-database
 DRIVER := postgres
-URI := postgres://schemahero:password@127.0.0.1:15432/schemahero?sslmode=disable
+POSTGRES_PASSWORD_URI := %21%40%23%24%25%5E%26%2A%28%29%7B%7D%27%22%3B
+URI := postgres://schemahero:$(POSTGRES_PASSWORD_URI)@127.0.0.1:15432/schemahero?sslmode=disable
 
 .PHONY: run
 run:
@@ -10,7 +11,8 @@ run:
 	docker pull postgres:$(PG_VERSION)
 	docker build -t $(DATABASE_IMAGE_NAME) .
 	@-docker rm -f $(DATABASE_CONTAINER_NAME) > /dev/null 2>&1 ||:
-	docker run -p 15432:5432 --rm -d --name $(DATABASE_CONTAINER_NAME) $(DATABASE_IMAGE_NAME)
+	# the $ is doubled due to make :-(
+	docker run -p 15432:5432 --rm -d -e POSTGRES_PASSWORD='!@#$$%^&*(){}'\''";' -e POSTGRES_HOST_AUTH_METHOD=md5 --name $(DATABASE_CONTAINER_NAME) $(DATABASE_IMAGE_NAME)
 	while ! docker exec $(DATABASE_CONTAINER_NAME) pg_isready --quiet; do sleep 1; done
 	@sleep 1
 
