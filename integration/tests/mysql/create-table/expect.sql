b3ab962db59a5772bd5f4b8cbc54a473aabb773d
divolgin <dmitriy@replicated.com>
2021-10-15 19:53:47 +0000

    Create tables with indexes if they are specified
    Signed-off-by: divolgin <dmitriy@replicated.com>

--- integration/tests/mysql/create-table/expect.sql
+++ integration/tests/mysql/create-table/expect.sql
@@ -1,1 +1,1 @@
-create table `users` (`id` int (11), `login` varchar (255), `name` varchar (255) not null default 'ethan', primary key (`id`));
+create table `users` (`id` int (11), `login` varchar (255), `name` varchar (255) not null default 'ethan', `email` varchar (255) not null, primary key (`id`), key idx_users_name (name), unique key idx_users_email (email));
