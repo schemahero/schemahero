03b92d6b159cd9c40d8a88f12f3d8a6d655e933b
divolgin <dmitriy@replicated.com>
2021-12-28 22:46:16 +0000

    Ignore hidden files when planning
    Signed-off-by: divolgin <dmitriy@replicated.com>

---
+++ pkg/files/files_windows.go
@@ -0,0 +1,20 @@
+//go:build windows
+// +build windows
+
+package files
+
+import (
+	"syscall"
+)
+
+func IsHidden(filename string) (bool, error) {
+	pointer, err := syscall.UTF16PtrFromString(filename)
+	if err != nil {
+		return false, err
+	}
+	attributes, err := syscall.GetFileAttributes(pointer)
+	if err != nil {
+		return false, err
+	}
+	return attributes&syscall.FILE_ATTRIBUTE_HIDDEN != 0, nil
+}
