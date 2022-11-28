16e86e88515119a9f27fc941a9f4a2ab6c49355e
Marc Campbell <marc.e.campbell@gmail.com>
2022-11-27 19:22:58 -0800

    Update tests to passing state
    Signed-off-by: Marc Campbell <marc.e.campbell@gmail.com>

--- integration/tests/mysql/column-set-default/expect.sql
+++ integration/tests/mysql/column-set-default/expect.sql
@@ -1,6 +1,6 @@
-alter table `users` modify column `account_type` varchar (10) null default "trial";
+alter table `users` modify column `account_type` varchar (10) collate utf8mb4_0900_ai_ci null default "trial";
 update `users` set `account_type`="trial" where `account_type` is null;
-alter table `users` modify column `account_type` varchar (10) not null default "trial";
+alter table `users` modify column `account_type` varchar (10) collate utf8mb4_0900_ai_ci not null default "trial";
 alter table `users` modify column `num_seats` int (11) null default "5";
 update `users` set `num_seats`="5" where `num_seats` is null;
 alter table `users` modify column `num_seats` int (11) not null default "5";
--- integration/tests/mysql/not-null/expect.sql
+++ integration/tests/mysql/not-null/expect.sql
@@ -1,1 +1,1 @@
-alter table `projects` modify column `icon_uri` varchar (255);
+alter table `projects` modify column `icon_uri` varchar (255) collate utf8mb4_0900_ai_ci;
