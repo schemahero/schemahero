6d6483784864afd230b7bef4d6777c64445bf9d3
ishaankalra <ishaan.kalra@facets.cloud>
2023-12-04 12:23:07 -0500

    feat: tolerations, nodeSelectors, and image from the database object
    Restore previous work from ishaankalra <ishaan.kalra@facets.cloud> which
    I had merged and then reverted due to drift in the underlying code and
    to improve error checking.
    
    Signed-off-by: Chris Gianelloni <cgianelloni@applause.com>

--- pkg/apis/databases/v1alpha4/database_types.go
+++ pkg/apis/databases/v1alpha4/database_types.go
@@ -31,9 +31,17 @@ type DatabaseConnection struct {
 	TimescaleDB *PostgresConnection    `json:"timescaledb,omitempty"`
 }
 
+type Toleration struct {
+	Effect   string `json:"effect,omitempty"`
+	Key      string `json:"key,omitempty"`
+	Operator string `json:"operator,omitempty"`
+	Value    string `json:"value,omitempty"`
+}
+
 type SchemaHero struct {
 	Image        string            `json:"image,omitempty"`
 	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+	Tolerations  []Toleration      `json:"tolerations,omitempty"`
 }
 
 type DatabaseSpec struct {
