be78d2f2413e121f664434acacaccf55b6040df6
Andrew Lavery <laverya@umich.edu>
2024-01-26 14:42:21 -0500

    two named indexes, one already existing
    Signed-off-by: Andrew Lavery <laverya@umich.edu>

--- integration/tests/postgres/ordered-index-create/expect.sql
+++ integration/tests/postgres/ordered-index-create/expect.sql
@@ -1,1 +1,1 @@
-create unique index idx_users_email on users (email);
+create index users_tz_priority_2 on users (tz_2 desc, tz_3 asc);
--- integration/tests/postgres/ordered-index-create/fixtures.sql
+++ integration/tests/postgres/ordered-index-create/fixtures.sql
@@ -2,5 +2,8 @@ create table users (
   id integer primary key not null,
   email varchar(255) not null,
   "tz_1" timestamp,
-  "tz_2" timestamp with time zone
+  "tz_2" timestamp,
+  "tz_3" timestamp
 );
+
+create index users_tz_priority_1 on users (tz_1 desc, tz_2 asc);
