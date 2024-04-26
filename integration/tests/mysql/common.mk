b7eeecca29e860a6ea154e6d9882e40899b99bd2
divolgin <dmitriy@replicated.com>
2024-04-26 18:40:47 +0000

    Better wait logic for mysql server in tests
    Signed-off-by: divolgin <dmitriy@replicated.com>

--- integration/tests/mysql/common.mk
+++ integration/tests/mysql/common.mk
@@ -2,15 +2,18 @@ SHELL := /bin/bash
 DATABASE_IMAGE_NAME := schemahero/database
 DATABASE_CONTAINER_NAME := schemahero-database
 DRIVER := mysql
-URI := schemahero:password@tcp(localhost:13306)/schemahero?tls=false
+USERNAME := schemahero
+PASSWORD := password
+DATABASE := schemahero
+URI := $(USERNAME):$(PASSWORD)@tcp(localhost:13306)/$(DATABASE)?tls=false
 
 .PHONY: run
 run:
 	# Fixtures
-	docker build -t $(DATABASE_IMAGE_NAME) .
-	@-docker rm -f $(DATABASE_CONTAINER_NAME) > /dev/null 2>&1 ||:
+	@-docker rm -f $(DATABASE_CONTAINER_NAME) > /dev/null 2>&1 || true
+	docker build --no-cache -t $(DATABASE_IMAGE_NAME) .
 	docker run -p 13306:3306 --rm -d --name $(DATABASE_CONTAINER_NAME) $(DATABASE_IMAGE_NAME)
-	while ! docker exec $(DATABASE_CONTAINER_NAME) mysqladmin ping --silent; do sleep 1; done
+	while ! docker exec $(DATABASE_CONTAINER_NAME) mysql -u$(USERNAME) -p$(PASSWORD) $(DATABASE) -N -s -e "show tables" 2> /dev/null; do sleep 1; done
 	@sleep 10
 
 	# Plan
@@ -18,10 +21,16 @@ run:
 
 	# Verify
 	@echo Verifying results for $(TEST_NAME)
-	diff -B expect.sql out.sql
+	if ! diff -B expect.sql out.sql; then \
+		docker logs $(DATABASE_CONTAINER_NAME); \
+		exit 1; \
+	fi
 
 	# Apply
-	../../../../bin/kubectl-schemahero apply --driver=$(DRIVER) --uri="$(URI)" --ddl out.sql
+	if ! ../../../../bin/kubectl-schemahero apply --driver=$(DRIVER) --uri="$(URI)" --ddl out.sql; then \
+		docker logs $(DATABASE_CONTAINER_NAME); \
+		exit 1; \
+	fi
 
 	# Cleanup
 	@-sleep 5
